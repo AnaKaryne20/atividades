026f3ce73d92b687be22aa4d2098468b8ae56e4d
AnaKaryne20 <anakaryne2013@outlook.com>
2014-01-13 18:29:48 -0200

    Create menu.php

---
+++ cadastrodeprodutos/menu.php
@@ -0,0 +1,12 @@
+<a href="index.php">Listar Produtos</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover</a>
+&nbsp;&nbsp;
+<a href="form_editar.php">Editar</a>
+
+<br/><br/>
+
+<fieldset>
+</center>

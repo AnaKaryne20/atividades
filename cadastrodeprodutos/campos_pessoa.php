5a765b417621ab35491de8ce932ed9ca6f0be274
AnaKaryne20 <anakaryne2013@outlook.com>
2014-01-13 18:26:04 -0200

    Create campos_pessoa.php

---
+++ cadastrodeprodutos/campos_pessoa.php
@@ -0,0 +1,39 @@
+<label>Nome do produto :</label>
+<input type="text" name="nome" />
+<br/>
+<input type="checkbox" name="aceito" value="sim" />
+<label>Produto Novo</label>
+<br/>
+<b>Cor do Produto</b><br/>
+Rosa <input type="radio" name="cor" value="Vermelho" /><br/>
+Verde <input type="radio" name="cor" value="amarelo"/><br/>
+Vermelho <input type="radio" name="cor" value="Vermelho"/><br/>
+
+
+<br/>
+
+Prazo de Entrega: <select name="prazo">
+    <option>5 Dias</option>
+    <option>10 Dias</option>
+    <option>15 Dias</option>
+</select>
+
+
+
+Tipo de Produto: <select name="tipo de produto">
+    <option>Produto Simples</option>
+    <option>Produto Configuravel</option>
+    <option>Produto Virtual </option>
+    
+    
+
+</select>
+
+
+
+<br/>
+<label>Detalhes</label>
+<br/>
+<textarea name="observacoes"></textarea>
+<br/>
+
